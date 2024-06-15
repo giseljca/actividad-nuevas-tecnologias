347197f3cd44a4ce10dab5a9c8b5d8e2e06f8411
giseljca <145206305+giseljca@users.noreply.github.com>
2024-06-14 21:48:16 -0500

    c-7

--- pages/3_Proyecto individual.py
+++ pages/3_Proyecto individual.py
@@ -1,6 +1,7 @@
 import streamlit as st # type: ignore
 import pandas as pd
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 
 st.set_page_config(layout="wide")
 st.title("Análisis de Datos Financieros")
@@ -19,6 +20,23 @@ st.header('Filtrar por precio de apertura')
 precio_apertura = st.number_input('Precio de Apertura mayor a:', value=0.0)
 filtered_open = df[df['Open'] > precio_apertura]
 st.dataframe(filtered_open)
+
+# Generar categorías de intervalos de precios
+intervalos = [0, 100, 200, 300, 400, 500, 600]
+labels = ['0-100', '100-200', '200-300', '300-400', '400-500', '500-600']
+filtered_open['Price_Range'] = pd.cut(filtered_open['Open'], bins=intervalos, labels=labels, include_lowest=True)
+
+# Contar las ocurrencias en cada intervalo
+counts = filtered_open['Price_Range'].value_counts().sort_index()
+
+# Crear el gráfico de pastel
+fig, ax = plt.subplots()
+ax.pie(counts, labels=counts.index, autopct='%1.1f%%', startangle=90)
+ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+# Mostrar el gráfico en Streamlit
+st.header('Gráfico de Pastel de Intervalos de Precio de Apertura')
+st.pyplot(fig)
 
 # Función de filtro por rango de fechas
 def filtro_por_rango_de_fechas():
